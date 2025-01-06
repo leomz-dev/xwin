5ad4905c03004454f2e6bcc5af4c162b0d216830
leonardo.meza.v18 <leonardo.meza.v18@gmail.com>
2025-01-06 22:14:45 +0000

    Xin create

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00ff00',
+        secondary: '#1a1a1a',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
